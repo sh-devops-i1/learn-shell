47662352c400b1a217989f3e8bcf1f2dd51f5801
shujath-git900 <shujath.devops@gmail.com>
2026-01-21 15:41:26 +0530

    for loop demo

--- 01_Loops.sh
+++ 01_Loops.sh
@@ -1,5 +1,11 @@
+# while loop works on expression
 i=0
 while [ $i -le 10 ]; do
     echo i - $i
     i=$((i+1))
+done
+
+# for loop
+for fruits in Apple Banana Orange Kiwi; do
+  echo $fruits
 done
